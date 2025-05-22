06ec784dc87f4fc5880a77740643fd80ca9519ac
André Luis Pleti <andre.pleti@hotmail.com.br>
2025-05-22 17:52:49 -0300

    Correções aplicadas no nginx e docker.

--- TesteRommanel.Frontend/src/app/services/http.service.ts
+++ TesteRommanel.Frontend/src/app/services/http.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { take } from 'rxjs/operators';
-import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,22 +10,22 @@ export class HttpService<T> {
   constructor(private http: HttpClient) { }
 
   Get(caminho: string) {
-    return this.http.get<T>(`${environment.apiUrl}${caminho}`).pipe(take(1));
+    return this.http.get<T>(caminho).pipe(take(1));
   }
 
   Listar(caminho: string) {
-    return this.http.get<T[]>(`${environment.apiUrl}${caminho}`).pipe(take(1));
+    return this.http.get<T[]>(caminho).pipe(take(1));
   }
 
   Post(caminho: string, corpo: object) {
-    return this.http.post<T>(`${environment.apiUrl}${caminho}`, corpo).pipe(take(1));
+    return this.http.post<T>(caminho, corpo).pipe(take(1));
   }
 
   Put(caminho: string, corpo: object) {
-    return this.http.put<T>(`${environment.apiUrl}${caminho}`, corpo).pipe(take(1));
+    return this.http.put<T>(caminho, corpo).pipe(take(1));
   }
 
   Delete(caminho: string) {
-    return this.http.delete(`${environment.apiUrl}${caminho}`).pipe(take(1));
+    return this.http.delete(caminho).pipe(take(1));
   }
 }
--- TesteRommanel.Frontend/src/app/views/cliente/cliente.service.ts
+++ TesteRommanel.Frontend/src/app/views/cliente/cliente.service.ts
@@ -12,30 +12,32 @@ export class ClienteService {
   
   constructor(private httpService: HttpService<Cliente>) { }
 
+  apiURL: string = "/api/cliente";
+
   ListarPor(nome: string) {
-    return this.httpService.Listar(`cliente?nome=${nome}`)
+    return this.httpService.Listar(`${this.apiURL}?nome=${nome}`)
     .pipe(map(x => {
       return x;
     }));
   }
 
   Get(id: number) {
-      return this.httpService.Get(`cliente/${id}`)
+      return this.httpService.Get(`${this.apiURL}/${id}`)
       .pipe(map(x => {
         return x;
     }));
   }
 
   Post(objeto: ClienteInserirCommand) {
-    return this.httpService.Post("cliente", objeto);
+    return this.httpService.Post(this.apiURL, objeto);
   }
 
   Put(objeto: ClienteAtualizarCommand) {
-    return this.httpService.Put("cliente", objeto);
+    return this.httpService.Put(this.apiURL, objeto);
   }
 
   Delete(id: number) {
-    return this.httpService.Delete(`cliente/${id}`);
+    return this.httpService.Delete(`${this.apiURL}/${id}`);
   }
 
 }
